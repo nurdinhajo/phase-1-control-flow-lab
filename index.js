f53b196e1b7f236c489f1618da082bb62d8826fc
nuru <nurudiin222@gmail.com>
2023-03-30 10:02:26 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
+//Test 1
+function scuberGreetingForFeet(feet){
   // Write your code here!
+  let response;
+  if  (feet <= 400){
+    response = 'This one is on me!';
+  }else if(feet > 2000 && feet <= 2500){
+    response ='I will gladly take your thirty bucks.';
+  }else if(feet > 2500){
+    response = 'No can do.';
+  }
+  return response;
 }
 
-function ternaryCheckCity(){
+
+//Test 2
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+
+//Test 3
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  
+  switch (tip){
+    case 'generous' : 
+      return "Thank you so much.";
+    case 'not as generous' : 
+      return "Thank you.";
+    default : 
+      return "Bye.";
+  }
 }
